9567dc39a4dec4d30557bbda63a9c249ad367df0
Lucas Carvalho de Medeiros <drigerm@hotmail.com>
2023-05-30 09:48:42 -0300

    Add files via upload

---
+++ src/components/Missions.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {
+          missions.map((p) => (
+            <MissionCard
+              key={ p.name }
+              name={ p.name }
+              year={ p.year }
+              country={ p.country }
+              destination={ p.destination }
+            />))
+        }
+      </div>
+    );
+  }
+}
+
+export default Missions;
